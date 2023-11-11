9565f8eb2416142eac9954a7caab6351c071ea93
Chris Copleston <chris@newwaveapps.com>
2023-11-11 10:18:54 +0000

    Github pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig, sharpImageService } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig, sharpImageService } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship-pro.web3templates.com",
+  site: 'https://github.com/chris-copleston',
   image: {
     service: sharpImageService(),
-    domains: ["source.unsplash.com", "images.unsplash.com"],
+    domains: ['source.unsplash.com', 'images.unsplash.com'],
   },
   integrations: [tailwind(), mdx(), sitemap()],
-});
+})
